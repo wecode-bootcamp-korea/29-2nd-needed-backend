4c70443f91210efc40837146f780e9f3be316205
shawn365 <eogkr.kim@gmail.com>
2022-02-16 11:20:47 +0900

    create: occupation category

--- recruitments/views.py
+++ recruitments/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views     import View
+from django.http      import JsonResponse
 
-# Create your views here.
+from .models          import *
+
+class CategoryView(View):
+    def get(self, request):
+        occupation_categories = OccupationCategory.objects.all()
+
+        results = [{
+            "id"               : category.id,
+            "name"             : category.name,
+            "subcategory_list" : [{
+                "id"           : subcategory.id,
+                "name"         : subcategory.name,                                 
+            } for subcategory in category.occupation_subcategories.all()]
+        } for category in occupation_categories]
+
+        return JsonResponse({"results" : results}, status = 200)
