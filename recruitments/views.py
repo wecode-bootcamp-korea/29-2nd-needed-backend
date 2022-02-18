feec519b982387cda0b48b68f2a0319558eb96d3
aerialslash <33915211+aerialslash0422@users.noreply.github.com>
2022-02-18 10:12:25 +0900

    [Add] 채용 정보 리스트

--- recruitments/views.py
+++ recruitments/views.py
@@ -1,7 +1,10 @@
-from django.views     import View
-from django.http      import JsonResponse
+from django.views        import View
+from django.http         import JsonResponse
+from django.db.models    import Q, Prefetch
 
-from .models          import *
+from recruitments.models import OccupationCategory,Recruitment
+from companies.models    import Company, Tag
+
 
 class CategoryView(View):
     def get(self, request):
@@ -17,3 +20,38 @@ class CategoryView(View):
         } for category in occupation_categories]
 
         return JsonResponse({"results" : results}, status = 200)
+
+class RecruitmentsList(View):
+    def get(self,request):
+        try:
+            filters = {
+                'category'    : 'occupation_subcategory__occupation_category__in',
+                'subcategory' : 'occupation_subcategory__in',
+                'tag'         : 'company__tag_companies__tag__in',
+                'country'     : 'company__detail_area__province__country__name__in',
+                'location'    : 'company__detail_area__province__name__in',
+                'detail'      : 'company__detail_area__name__in'
+            }
+            
+            filter_set = {
+                filters.get(key) : value for (key,value) in dict(request.GET).items() if filters.get(key)
+            }
+            
+            recruitments = Recruitment.objects \
+                .order_by(request.GET.get("sort",'id')) \
+                .select_related('company__detail_area__province__country') \
+                .prefetch_related('company__tag_companies__tag') \
+                .filter(**filter_set)
+                
+            result = [{
+                "id"           : recruitment.id,
+                "name"         : recruitment.name,
+                "company_name" : recruitment.company.name,
+                "country"      : recruitment.company.detail_area.province.country.name,
+                "province"     : recruitment.company.detail_area.province.name,
+            }for recruitment in recruitments]
+        
+            return JsonResponse({'message':'SUCCESS' ,'Recruitment' : result}, status=200)
+        
+        except ValueError:
+            return JsonResponse({'message':'VALUE_ERROR'}, status=400)
