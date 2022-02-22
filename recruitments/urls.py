5b5942b98f62f300b2c72d0bd50233f6e92102f5
aerialslash <33915211+aerialslash0422@users.noreply.github.com>
2022-02-22 19:22:43 +0900

    [Add] 채용 상세 정보

--- recruitments/urls.py
+++ recruitments/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from recruitments.views      import CategoryView, RecruitmentsList
+from recruitments.views      import CategoryView, RecruitmentsList, RecruitmentsDetailView
 
 urlpatterns = [
     path("/categories", CategoryView.as_view()),
     path("", RecruitmentsList.as_view()),
+    path("/<int:recruitment_id>",RecruitmentsDetailView.as_view()),
 ]
