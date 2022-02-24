f47642cb114bfcb157dbc38b0643ffec23603c3a
shawn365 <eogkr.kim@gmail.com>
2022-02-24 11:20:50 +0900

    modify: resume, application

--- resumes/models.py
+++ resumes/models.py
@@ -3,6 +3,7 @@ from core.models import TimeStampModel
 
 class Resume(TimeStampModel):
     document = models.URLField(max_length=500)
+    name     = models.CharField(max_length=100)
     user     = models.ForeignKey('users.SocialLogin', on_delete=models.CASCADE, related_name='resumes')
     
     class Meta:
