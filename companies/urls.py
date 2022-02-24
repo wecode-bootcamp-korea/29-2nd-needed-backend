385def1f2e364a01a9e7cad113c10942487439b8
shawn365 <eogkr.kim@gmail.com>
2022-02-24 14:30:01 +0900

    modify: search

--- companies/urls.py
+++ companies/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from companies.views      import CompanyView
+from companies.views      import CompanyView, CompanySearchView
 
 urlpatterns = [
     path("/<int:company_id>", CompanyView.as_view()),
+    path("", CompanySearchView.as_view()),
 ]
